862f3eaba58d9c9d983ed6adca4a36a1e7deb327
charmerin <charm179@naver.com>
2024-07-28 22:34:06 +0900

    modal button modify

--- portfolio.js
+++ portfolio.js
@@ -13,28 +13,67 @@ function createUnderline(e) {
     e.currentTarget.style.color = "rgb(254, 151, 49)";
 }
 
-// modal
-function modalOpen(title, image, subImage1, subImage2, subImage3, subImage4) {
+// modal open
+function modalOpen(title, detail_id, image, subImage1, subImage2, subImage3, subImage4) {
     let insertTag = '<div style="display: flex;"><div style="flex: 1; text-align: center; color:#000; font-size: 2.5em; font-weight: bold;">' + title + '</div>'
-                    + '<div style="flex: 0.01; font-size: 3em; font-weight: bold; cursor: pointer; transform: translateY(-20px);">X</div></div>'
-                    + '<img src="' + image + '">';
+                    + '<div id="modal_close" style="flex: 0.01; color: #000; font-size: 3em; font-weight: bold; cursor: pointer; transform: translateY(-20px);">X</div></div>'
+                    + '<div class="modal_detail">'
+                    + '   <div><img src="' + image + '"></div>'
+                    + '   <div>' + document.querySelector('#' + detail_id + '_0').innerHTML + '</div>'
+                    + '</div>';
+
+
     if (subImage1 != null) {
-        insertTag += '<hr><img src="' + subImage1 + '">'
-                    + '<hr><img src="' + subImage2 + '">'
-                    + '<hr><img src="' + subImage3 + '">'
-                    + '<hr><img src="' + subImage4 + '">';
+        insertTag += '<hr><div class="modal_detail">'
+                    + '<div><img src="' + subImage1 + '"></div>'
+                    + '   <div>' + document.querySelector('#' + detail_id + '_1').innerHTML + '</div>'
+                    + '</div>'
+                    + '<hr><div class="modal_detail">'
+                    + '<div><img src="' + subImage2 + '"></div>'
+                    + '   <div>' + document.querySelector('#' + detail_id + '_2').innerHTML + '</div>'
+                    + '</div>'
+                    + '<hr><div class="modal_detail">'
+                    + '<div><img src="' + subImage3 + '"></div>'
+                    + '   <div>' + document.querySelector('#' + detail_id + '_3').innerHTML + '</div>'
+                    + '</div>'
+                    + '<hr><div class="modal_detail">'
+                    + '<div><img src="' + subImage4 + '"></div>'
+                    + '   <div>' + document.querySelector('#' + detail_id + '_4').innerHTML + '</div>'
+                    + '</div>';
+                    // + '<hr><img src="' + subImage2 + '">'
+                    // + '<hr><img src="' + subImage3 + '">'
+                    // + '<hr><img src="' + subImage4 + '">';
     }
 
     $('body').css('overflow', 'hidden');
-    $('.modal_contents').append(insertTag);
     $('.modal').addClass('modal_open');
+    $('.modal_contents').append(insertTag);
+
+    // modal close (button)
+    $('#modal_close').on('click', function() {
+        console.log('click');
+        $('body').css('overflow', 'auto');
+        $('.modal_contents').empty();
+        $('.modal').removeClass('modal_open');
+    });
 }
 
-$('.modal').on('click', function() {
-    $('body').css('overflow', 'auto');
-    $('.modal_contents').empty();
-    $('.modal').removeClass('modal_open');
+// modal close (past)
+// $('.modal').on('click', function() {
+//     $('body').css('overflow', 'auto');
+//     $('.modal_contents').empty();
+//     $('.modal').removeClass('modal_open');
+// });
+
+// modal close (current)
+$('.modal').on('click', function(e) {
+    if (e.target.className == "modal modal_open") {
+        $('body').css('overflow', 'auto');
+        $('.modal_contents').empty();
+        $('.modal').removeClass('modal_open');
+    }
 });
+
 
 // dday
 $(function() {
